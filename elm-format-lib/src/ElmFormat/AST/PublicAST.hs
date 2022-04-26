37f270c779a93af17abe43591a03948d897cd1e1
jefrww <janerwi@aol.com>
2022-04-26 04:39:02 +0200

    Add Type to Export to use CustomTypeVariant

--- elm-format-lib/src/ElmFormat/AST/PublicAST.hs
+++ elm-format-lib/src/ElmFormat/AST/PublicAST.hs
@@ -4,6 +4,7 @@ module ElmFormat.AST.PublicAST
     , module ElmFormat.AST.PublicAST.Module
     , module ElmFormat.AST.PublicAST.MaybeF
     , module ElmFormat.AST.PublicAST.Expression
+    , module ElmFormat.AST.PublicAST.Type
     ) where
 
 import ElmFormat.AST.PublicAST.Core (ToPublicAST(..), FromPublicAST(..), LocatedIfRequested(..))
@@ -11,3 +12,4 @@ import ElmFormat.AST.PublicAST.Config
 import ElmFormat.AST.PublicAST.Module (fromModule, toModule, Module(..), TopLevelStructure(..))
 import ElmFormat.AST.PublicAST.MaybeF
 import ElmFormat.AST.PublicAST.Expression
+import ElmFormat.AST.PublicAST.Type
