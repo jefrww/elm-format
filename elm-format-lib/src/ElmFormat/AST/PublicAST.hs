2a6a46070de1eecf47c3254839d156f60becec1b
jefrww <janerwi@aol.com>
2022-04-26 03:56:25 +0200

    Add Export Expression to use Definition

--- elm-format-lib/src/ElmFormat/AST/PublicAST.hs
+++ elm-format-lib/src/ElmFormat/AST/PublicAST.hs
@@ -3,9 +3,11 @@ module ElmFormat.AST.PublicAST
     , module ElmFormat.AST.PublicAST.Config
     , module ElmFormat.AST.PublicAST.Module
     , module ElmFormat.AST.PublicAST.MaybeF
+    , module ElmFormat.AST.PublicAST.Expression
     ) where
 
 import ElmFormat.AST.PublicAST.Core (ToPublicAST(..), FromPublicAST(..), LocatedIfRequested(..))
 import ElmFormat.AST.PublicAST.Config
 import ElmFormat.AST.PublicAST.Module (fromModule, toModule, Module(..), TopLevelStructure(..))
 import ElmFormat.AST.PublicAST.MaybeF
+import ElmFormat.Ast.PublicAst.Expression
